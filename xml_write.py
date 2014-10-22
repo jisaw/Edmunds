8b8de7a27ef98c0cf8f69a63ca0944035e99f533
Jake Sawyer <jisawyer@iastate.edu>
2014-10-21 22:32:53 -0500

    Bug fix

--- xml_write.py
+++ xml_write.py
@@ -55,7 +55,7 @@ def makexml(dataAngel, url, folder):
 	os.system("mkdir %s" % folder)
 	os.chdir(current_dir)
 
-	f = open(prefix + name, 'a')
+	f = open(name, 'w')
 	with f:
 		tree = ET.ElementTree(thread)
 		#tree = prettify(tree)
