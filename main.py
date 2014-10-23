8c94fe6ebefba8d31c4af70982aaaa6149f3b9cb
Jake Sawyer <jisawyer@iastate.edu>
2014-10-22 20:51:12 -0500

    Bug fix

--- main.py
+++ main.py
@@ -10,128 +10,127 @@ from xml_output import xmlout
 
 folder = sys.argv[1]
 
+
 def main():
-	for url in constants.start_urls:
-		urls = []
-		r = requests.get(url)
-		data = r.text
-		soup = BeautifulSoup(data)
-		lp = soup.find('a', class_ = re.compile('^LastPage'))
-		try:
-			for i in range(int(lp.string)):
-				new_num = lp['href'].find('=')+1
-				new_end = lp['href'][:new_num] + 'p%s&' % (i+1) 
-				new_url = 'http://forums.edmunds.com' + new_end
-				if new_url not in urls:
-					print(new_url)
-					urls.append(new_url)
-				else: 
-					print('Duplicate Link')
-		except:
-			print('\n\n  ERROR  \n\n')
-		metaDataExtraction(urls)
+  for url in constants.start_urls:
+    urls = []
+    r = requests.get(url)
+    data = r.text
+    soup = BeautifulSoup(data)
+    lp = soup.find('a', class_=re.compile('^LastPage'))
+    try:
+      for i in range(int(lp.string)):
+        new_num = lp['href'].find('=') + 1
+        new_end = lp['href'][:new_num] + 'p%s&' % (i + 1)
+        new_url = 'http://forums.edmunds.com' + new_end
+        if new_url not in urls:
+          print(new_url)
+          urls.append(new_url)
+        else:
+          print('Duplicate Link')
+    except:
+      print('\n\n  ERROR  \n\n')
+    metaDataExtraction(urls)
+
 
 def metaDataExtraction(urls):
-	for url in urls:
-		print(url)
-		r = requests.get(url)
-		data = r.text
-		soup = BeautifulSoup(data)
+  for url in urls:
+    print(url)
+    r = requests.get(url)
+    data = r.text
+    soup = BeautifulSoup(data)
 
-		original_posters = tl.get_original_posters(soup)
-	
-		dates = tl.get_date(soup)
-		date_title = dates[0]
-		lp_title = dates[1]
-	
-		datetimes = tl.get_datetimes(soup)
-		date_dt = datetimes[0]
-		lp_dt = datetimes[1]
-	
-		nums = tl.get_nums(soup)
-		replies = nums[0]
-		views = nums[1]
+    original_posters = tl.get_original_posters(soup)
 
-		threadUrls = tl.get_post_level(soup)
-	
-		dataAngel = xmlout()
-	
-		for i in range(len(date_title)):
-			print(original_posters[i])
-			dataAngel.set_orig_poster(original_posters[i])
-			print(date_title[i])
-			dataAngel.set_posted_at_date(date_title[i])
-			print(date_dt[i])
-			dataAngel.set_posted_at_datetime(date_dt[i])
-			print(views[i])
-			dataAngel.set_views(views[i])
-			print(replies[i])
-			dataAngel.set_replies(replies[i])
-			print(lp_title[i])
-			dataAngel.set_lastposted_date(lp_title[i])
-			print(lp_dt[i])
-			dataAngel.set_lastposted_datetime(lp_dt[i])
-			print(threadUrls[i])
-			postPageExtraction(threadUrls[i], dataAngel)
+    dates = tl.get_date(soup)
+    date_title = dates[0]
+    lp_title = dates[1]
 
-		
+    datetimes = tl.get_datetimes(soup)
+    date_dt = datetimes[0]
+    lp_dt = datetimes[1]
 
+    nums = tl.get_nums(soup)
+    replies = nums[0]
+    views = nums[1]
+
+    threadUrls = tl.get_post_level(soup)
+
+    dataAngel = xmlout()
+
+    for i in range(len(date_title)):
+      print(original_posters[i])
+      dataAngel.set_orig_poster(original_posters[i])
+      print(date_title[i])
+      dataAngel.set_posted_at_date(date_title[i])
+      print(date_dt[i])
+      dataAngel.set_posted_at_datetime(date_dt[i])
+      print(views[i])
+      dataAngel.set_views(views[i])
+      print(replies[i])
+      dataAngel.set_replies(replies[i])
+      print(lp_title[i])
+      dataAngel.set_lastposted_date(lp_title[i])
+      print(lp_dt[i])
+      dataAngel.set_lastposted_datetime(lp_dt[i])
+      print(threadUrls[i])
+      postPageExtraction(threadUrls[i], dataAngel)
 
 
 def postPageExtraction(url, dataAngel):
-	urls = []
-	r = requests.get(url)
-	print(url)
-	data = r.text
-	soup = BeautifulSoup(data)
-	lp = soup.find('a', class_ = re.compile('^LastPage'))
-	try:
-		for i in range(int(lp.string)):
-			link = lp['href']
-			print(link)
-			num = len(lp.string) + 1
-			if num > 300:
-				break
-			new_url = link[:-num] + 'p%s' % (i+1) 
-			if new_url not in urls:
-				print(new_url)
-				urls.append(new_url)
-			else: 
-				print('Duplicate Link')
-	except:
-		print('\n\n  ERROR  \n\n')
-	tags = pl.get_tags(soup)
-	dataAngel.set_tags(tags)
+  urls = []
+  r = requests.get(url)
+  print(url)
+  data = r.text
+  soup = BeautifulSoup(data)
+  lp = soup.find('a', class_=re.compile('^LastPage'))
+  try:
+    for i in range(int(lp.string)):
+      link = lp['href']
+      print(link)
+      num = len(lp.string) + 1
+      if num > 300:
+        break
+      new_url = link[:-num] + 'p%s' % (i + 1)
+      if new_url not in urls:
+        print(new_url)
+        urls.append(new_url)
+      else:
+        print('Duplicate Link')
+  except:
+    print('\n\n  ERROR  \n\n')
+  tags = pl.get_tags(soup)
+  dataAngel.set_tags(tags)
 
-	postExtraction(urls, dataAngel)
+  postExtraction(urls, dataAngel)
 
 
 def postExtraction(urls, dataAngel):
-	posts = []
-	try: 
-		for url in urls:
-			r = requests.get(url)
-			print('Got url: ' + url)
-			data = r.text
-			soup = BeautifulSoup(data)
-	
-			bodies = pl.get_post_body(soup)
-			print('Got bodies')
-			users = pl.get_post_user(soup)
-			print('Got users')
-			dates = pl.get_post_date(soup)
-			print('Got dates')
-			for i in range(len(bodies)):
-				posts.append([users[i], dates[i], bodies[i]])
-	except:
-		print("\n\nERROR\n\n")
-	dataAngel.set_posts(posts)
-	xw.makexml(dataAngel, url, folder)
+  posts = []
+  try:
+    for url in urls:
+      r = requests.get(url)
+      print('Got url: ' + url)
+      data = r.text
+      soup = BeautifulSoup(data)
+
+      bodies = pl.get_post_body(soup)
+      print('Got bodies')
+      users = pl.get_post_user(soup)
+      print('Got users')
+      dates = pl.get_post_date(soup)
+      print('Got dates')
+      for i in range(len(bodies)):
+        posts.append([users[i], dates[i], bodies[i]])
+  except:
+    print("\n\nERROR\n\n")
+  dataAngel.set_posts(posts)
+  xw.makexml(dataAngel, url, folder)
 
 
-#def threadLevelSoup(soup, url):
-#	soups = []
-#	urls = []
+# def threadLevelSoup(soup, url):
+# soups = []
+# urls = []
 #	for url in tl.get_post_level(soup):
 #		r = requests.get(url)
 #		data = r.text
@@ -212,4 +211,4 @@ def postExtraction(urls, dataAngel):
 
 
 if __name__ == "__main__":
-	main()#
+  main()  #
--- xml_output.py
+++ xml_output.py
@@ -1,59 +1,76 @@
 class xmlout:
-	#orig_poster = ""
-	#posted_at_date = ""
-	#posted_at_datetime = ""
-	#views = ""
-	#replies = ""
-	#lastposted_date = ""
-	#lasposted_datetime = ""
-	#tags = ""
-	#posts = ""
+  # orig_poster = ""
+  # posted_at_date = ""
+  #posted_at_datetime = ""
+  #views = ""
+  #replies = ""
+  #lastposted_date = ""
+  #lasposted_datetime = ""
+  #tags = ""
+  #posts = ""
 
 
-	def __init__(self):
-		self.orig_poster  = ""
-		self.posted_at_date  = ""
-		self.posted_at_datetime  = ""
-		self.views  = ""
-		self.replies  = ""
-		self.lastposted_date  = ""
-		self.lasposted_datetime  = ""
-		self.tags  = []
-		self.posts  = []
+  def __init__(self):
+    self.orig_poster = ""
+    self.posted_at_date = ""
+    self.posted_at_datetime = ""
+    self.views = ""
+    self.replies = ""
+    self.lastposted_date = ""
+    self.lasposted_datetime = ""
+    self.tags = []
+    self.posts = []
 
-	def set_orig_poster(self, value):
-		self.orig_poster = value
-	def set_posted_at_date(self, value):
-		self.posted_at_date = value
-	def set_posted_at_datetime(self, value):
-		self.posted_at_datetime = value
-	def set_views(self, value):
-		self.views = value
-	def set_replies(self, value):
-		self.replies = value
-	def set_lastposted_date(self, value):
-		self.lastposted_date = value
-	def set_lastposted_datetime(self, value):
-		self.lasposted_datetime = value
-	def set_tags(self, value):
-		self.tags = value
-	def set_posts(self, value):
-		self.posts = value	
-	def get_orig_poster(self):
-		return self.orig_poster
-	def get_posted_at_date(self):
-		return self.posted_at_date
-	def get_posted_at_datetime(self):
-		return self.posted_at_datetime
-	def get_views(self):
-		return self.views
-	def get_replies(self):
-		return self.replies
-	def get_lastposted_date(self):
-		return self.lastposted_date
-	def get_lasposted_datetime(self):
-		return self.lasposted_datetime
-	def get_tags(self):
-		return self.tags
-	def get_posts(self):
-			return self.posts
+  def set_orig_poster(self, value):
+    self.orig_poster = value
+
+  def set_posted_at_date(self, value):
+    self.posted_at_date = value
+
+  def set_posted_at_datetime(self, value):
+    self.posted_at_datetime = value
+
+  def set_views(self, value):
+    self.views = value
+
+  def set_replies(self, value):
+    self.replies = value
+
+  def set_lastposted_date(self, value):
+    self.lastposted_date = value
+
+  def set_lastposted_datetime(self, value):
+    self.lasposted_datetime = value
+
+  def set_tags(self, value):
+    self.tags = value
+
+  def set_posts(self, value):
+    self.posts = value
+
+  def get_orig_poster(self):
+    return self.orig_poster
+
+  def get_posted_at_date(self):
+    return self.posted_at_date
+
+  def get_posted_at_datetime(self):
+    return self.posted_at_datetime
+
+  def get_views(self):
+    return self.views
+
+  def get_replies(self):
+    return self.replies
+
+  def get_lastposted_date(self):
+    return self.lastposted_date
+
+  def get_lasposted_datetime(self):
+    return self.lasposted_datetime
+
+  def get_tags(self):
+    return self.tags
+
+  def get_posts(self):
+    return self.posts
