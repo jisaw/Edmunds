274712b6ff87d8bb88c58f953fea96afb4a2d471
Jake Sawyer <jisawyer@iastate.edu>
2014-11-03 11:40:58 -0600

    Treid some things to speed up scraping

--- xml_write.py
+++ xml_write.py
@@ -2,70 +2,70 @@ import xml.etree.cElementTree as ET
 from xml.dom import minidom
 import os
 
+
 def makexml(dataAngel, url, folder, make):
-	#Formats the name for the file
-	name = '%s.xml' % url[7:]
-	name = name.replace("/", "_")
+    # Formats the name for the file
+    name = '%s.xml' % url[7:]
+    name = name.replace("/", "_")
 
+    prefix = '../../data/%s/%s/' % (folder, make)
+    #prefix = ''
+    t = dataAngel.get_tags()
+    p = dataAngel.get_posts()
 
-	prefix = '../../data/%s/%s/' % (folder, make)
-	#prefix = ''
-	t = dataAngel.get_tags()
-	p = dataAngel.get_posts()
+    #This is the creation of the XML tree
+    thread = ET.Element('thread')
 
-	#This is the creation of the XML tree
-	thread = ET.Element('thread')
+    orig_poster = ET.SubElement(thread, "orig_poster")
+    orig_poster.text = dataAngel.get_orig_poster()
 
-	orig_poster = ET.SubElement(thread, "orig_poster")
-	orig_poster.text = dataAngel.get_orig_poster()
+    posted_at_date = ET.SubElement(thread, "posted_at_date")
+    posted_at_date.text = dataAngel.get_posted_at_date()
 
-	posted_at_date = ET.SubElement(thread, "posted_at_date")
-	posted_at_date.text =dataAngel.get_posted_at_date()
+    posted_at_datetime = ET.SubElement(thread, "posted_at_datetime")
+    posted_at_datetime.text = dataAngel.get_posted_at_datetime()
 
-	posted_at_datetime = ET.SubElement(thread, "posted_at_datetime")
-	posted_at_datetime.text = dataAngel.get_posted_at_datetime()
+    views = ET.SubElement(thread, "views")
+    views.text = dataAngel.get_views()
 
-	views = ET.SubElement(thread, "views")
-	views.text = dataAngel.get_views()
+    replies = ET.SubElement(thread, "replies")
+    replies.text = dataAngel.get_replies()
 
-	replies = ET.SubElement(thread, "replies")
-	replies.text = dataAngel.get_replies()
+    lastposted_date = ET.SubElement(thread, "lastposted_date")
+    lastposted_date.text = dataAngel.get_lastposted_date()
 
-	lastposted_date = ET.SubElement(thread, "lastposted_date")
-	lastposted_date.text = dataAngel.get_lastposted_date()
+    lasposted_datetime = ET.SubElement(thread, "lasposted_datetime")
+    lasposted_datetime.text = dataAngel.get_lasposted_datetime()
 
-	lasposted_datetime = ET.SubElement(thread, "lasposted_datetime")
-	lasposted_datetime.text = dataAngel.get_lasposted_datetime()
+    tags = ET.SubElement(thread, "tags")
+    tag_list = []
+    for i in range(len(t)):
+        tag_list.append(t[i])
+    tags.text = ", ".join(tag_list)
 
-	tags = ET.SubElement(thread, "tags")
-	tag_list = []
-	for i in range(len(t)):
-		tag_list.append(t[i])
-	tags.text = ", ".join(tag_list)
+    posts = ET.SubElement(thread, "posts")
 
-	posts = ET.SubElement(thread, "posts")
+    for i in range(len(p)):
+        username = ET.SubElement(posts, "username")
+        username.text = p[i][0]
 
-	for i in range(len(p)):
-		username = ET.SubElement(posts, "username")
-		username.text = p[i][0]
-	
-		date = ET.SubElement(posts, "date")
-		date.text = p[i][1]
-	
-		body = ET.SubElement(posts, "body")
-		body.text = p[i][2]
+        date = ET.SubElement(posts, "date")
+        date.text = p[i][1]
 
+        body = ET.SubElement(posts, "body")
+        body.text = p[i][2]
 
-	f = open(prefix + name, 'w')
-	with f:
-		tree = ET.ElementTree(thread)
-		#tree = prettify(tree)
-		tree.write(f)
-		print("\nXML WRITTEN!!")
-	f.close()
+    f = open(prefix + name, 'w')
+    with f:
+        tree = ET.ElementTree(thread)
+        #tree = prettify(tree)
+        tree.write(f)
+        print("\nXML WRITTEN!!")
+    f.close()
+    dataAngel = ''
 
-#A method to prettify the XML --Curently not in use
+# A method to prettify the XML --Curently not in use
 def prettify(elem):
-	rough_string = ET.tostring(elem, 'utf-8')
-	reparsed = minidom.parseString(rough_string)
-	return reparsed.toprettyxml(indent=" ")
+    rough_string = ET.tostring(elem, 'utf-8')
+    reparsed = minidom.parseString(rough_string)
+    return reparsed.toprettyxml(indent=" ")
