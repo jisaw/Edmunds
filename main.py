88524c637c212c299dc8bc585960911e739c3bc8
Jake Sawyer <jisawyer@iastate.edu>
2014-11-02 21:55:15 -0600

    Added timestamps

--- main.py
+++ main.py
@@ -9,10 +9,11 @@ from xml_output import xmlout
 from urllib import urlopen
 import os
 import lxml
+import datetime
 
 
 #Takes in the command line argument immediatly after main.py and creates
-#a folder with that name in the data directory 
+#a folder with that name in the data directory
 folder = sys.argv[1]
 
 current_dir = os.getcwd()
@@ -23,37 +24,58 @@ os.chdir(current_dir)
 
 #This starts with the constant urls and creates a list of all the thread level pages
 def main():
+  print('starting main')
+  print datetime.datetime.now().strftime('%H:%M:%S')
   dataAngel = xmlout()
   i = 0
   for i in range(len(constants.start_urls)):
   #for url in constants.start_urls:
+    print('for i in range(len(constants.start_urls))')
+    print datetime.datetime.now().strftime('%H:%M:%S')
     make = constants.MAKES[i]
     os.chdir("/home/research/projects/edmunds/data/%s" % folder)
     os.system("mkdir %s" % make)
     os.chdir(current_dir)
 
     urls = []
+    print('Crated make folder')
+    print datetime.datetime.now().strftime('%H:%M:%S')
     r = urlopen(constants.start_urls[i])
     soup = BeautifulSoup(r, 'lxml')
+    print('soup opened')
+    print datetime.datetime.now().strftime('%H:%M:%S')
     lp = soup.find('a', class_=re.compile('^LastPage'))
     try:
       for i in range(int(lp.string)):
+        print('with lp try')
+        print datetime.datetime.now().strftime('%H:%M:%S')
         new_num = lp['href'].find('=') + 1
         new_end = lp['href'][:new_num] + 'p%s&' % (i + 1)
         new_url = 'http://forums.edmunds.com' + new_end
+        print('Created new url')
+        print datetime.datetime.now().strftime('%H:%M:%S')
         if new_url not in urls:
           print(new_url)
           urls.append(new_url)
+          print('Added URL')
+          print datetime.datetime.now().strftime('%H:%M:%S')
         else:
           print('Duplicate Link')
     except:
       print('\n\n  ERROR  \n\n')
+      print datetime.datetime.now().strftime('%H:%M:%S')
+    print('calling metaDataExtraction')
+    print datetime.datetime.now().strftime('%H:%M:%S')
     metaDataExtraction(urls, dataAngel, make)
+    print('metaDataExtraction finished')
+    print datetime.datetime.now().strftime('%H:%M:%S')
 
 #Is run on every thread level page. it scrapes the meta data, adds it to the dataAngel class, and then passes the links along for the post level scraping
 def metaDataExtraction(urls, dataAngel, make):
   for url in urls:
     print(url)
+    print('getting meta data')
+    print datetime.datetime.now().strftime('%H:%M:%S')
     r = urlopen(url)
     soup = BeautifulSoup(r, 'lxml')
 
@@ -90,7 +112,11 @@ def metaDataExtraction(urls, dataAngel, make):
       print(lp_dt[i])
       dataAngel.set_lastposted_datetime(lp_dt[i])
       print(threadUrls[i])
+      print('calling postPageExtraction')
+      print datetime.datetime.now().strftime('%H:%M:%S')
       postPageExtraction(threadUrls[i], dataAngel, make)
+      print('finished postPageExtraction')
+      print datetime.datetime.now().strftime('%H:%M:%S')
 
 #This is where all of the post level pages are added to a list to be iterated over. Also the tags are scraped and stored in the dataAngel class with the other information
 def postPageExtraction(url, dataAngel, make):
@@ -117,7 +143,11 @@ def postPageExtraction(url, dataAngel, make):
   tags = pl.get_tags(soup)
   dataAngel.set_tags(tags)
 
+  print('calling postExtraction')
+  print datetime.datetime.now().strftime('%H:%M:%S')
   postExtraction(urls, dataAngel, url, make)
+  print('postExtraction finished')
+  print datetime.datetime.now().strftime('%H:%M:%S')
 
 #Finally. this is where the posts themselves are scraped. They are also added to the dataAngel class and passed to the xml writing script.
 def postExtraction(urls, dataAngel, name_url, make):
@@ -140,7 +170,11 @@ def postExtraction(urls, dataAngel, name_url, make):
   except:
     print("\n\nERROR\n\n")
   dataAngel.set_posts(posts)
+  print('calling makexml')
+  print datetime.datetime.now().strftime('%H:%M:%S')
   xw.makexml(dataAngel, name_url, folder, make)
+  print('xml written')
+  print datetime.datetime.now().strftime('%H:%M:%S')
 
 if __name__ == "__main__":
   main()  #
